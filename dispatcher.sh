52a463718d724b9a090bc76b00e9bea82cf9008a
TangliziGit <tanglizimail@foxmail.com>
2019-12-16 23:17:09 +0800

    add logging function

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,3 +1,7 @@
+function log() {
+    printf "$1\n\n" | sed -u 's//\n/g' >> $2
+}
+
 if test ! -p .pipe; then
     mkfifo .pipe
 fi
@@ -17,6 +21,10 @@ while [ $i -lt 7 ]; do
         printf "HTTP/1.0 200 OK\r\n"
         printf "Content-Length: %d\r\n\r\n" "$(echo -n $resp | wc -c)"
         printf "$resp"
+
+        log "$req" "req.log"
+        log "$resp" "resp.log"
+
     } | sudo nc -clvp 80 > .pipe
 
     i=$(( $i+1 ))
