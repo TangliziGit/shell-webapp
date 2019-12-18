613b59e4bd962632d5ba54d08195b31fe4ffa56f
TangliziGit <tanglizimail@foxmail.com>
2019-12-18 18:29:20 +0800

    add unit testing process

--- README.md
+++ README.md
@@ -15,6 +15,12 @@ To run the server:
 To run a client, you can open a browser and just go `http://localhost`.  
 Or you can `curl localhost`
 
+## Unit Test
+Requires [bats](https://github.com/bats-core/bats-core) to process unit testing.
+```bash
+bats test/*.sh
+```
+
 ## Todo
 
 - [x] web server
@@ -24,6 +30,7 @@ Or you can `curl localhost`
 - [x] forward and redirect
 - [x] custom HTTP header
 - [x] HTTP status table
+- [x] unit testing for handlers
 - [ ] CI/CD
 - [ ] README: explain how it works
 - [ ] learn how to write shell
---
+++ test/testHandler.sh
@@ -0,0 +1,27 @@
+sample="test/sample"
+header="$(cat $sample/.header.txt)"
+
+@test "test handler/index" {
+    req="GET /index HTTP/1.0\r\n${header}"
+    run handler/index.sh "$req"
+    [ "$output" = "$(cat $sample/index.txt)" ]
+}
+
+@test "test handler/header" {
+    req="GET /header HTTP/1.0\r\n${header}"
+    run handler/header.sh "$req"
+    [ "$output" = "$(cat $sample/header.txt)" ]
+}
+
+@test "test handler/forward" {
+    req="GET /forward HTTP/1.0\r\n${header}"
+    run handler/forward.sh "$req"
+    [ "$output" = "$(cat $sample/forward.txt)" ]
+}
+
+@test "test handler/redirect" {
+    req="GET /redirect HTTP/1.0\r\n${header}"
+    run handler/redirect.sh "$req"
+    [ "$output" = "$(cat $sample/redirect.txt)" ]
+}
+
